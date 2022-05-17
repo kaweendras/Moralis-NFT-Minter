88a318ba0f09827d5cd99c86e288b209122d5b4b
kaweendras <salitha.wijerathna@my.sliit.lk>
2022-05-17 10:14:18 +0530

    new updates

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,9 +1,15 @@
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { useMoralis } from 'react-moralis'
+import Moralis from 'moralis' 
+import Web3 from 'web3'
+import { contractABI, contractAddress } from '../../contract'
+const buffer = require('buffer').Buffer;
+
+const web3 = new Web3(Web3.givenProvider)
 
 function Dashboard() {
-    const {isAuthenticated, logout} = useMoralis()
+    const {isAuthenticated, logout, user} = useMoralis()
     const router = useRouter()
     const [name, setname] = useState('')
     const [description, setdescription] = useState('')
@@ -17,6 +23,39 @@ function Dashboard() {
 
     const onSubmit = async (e) => {
         e.preventDefault()
+        try {
+            // save images to ipfs
+            const file1 = new Moralis.File(file.name, file)
+            await file1.saveIPFS()
+            const file1Url = file1.ipfs()
+
+            //generate metadata and save to ipfs
+            const metadata = {
+                name: name,
+                description: description,
+                image: file1Url
+                }
+            const file2 = new Moralis.File(`${name}metadata.json`,{
+                base64: buffer.from(JSON.stringify(metadata)).toString('base64')
+            })
+            await file2.saveIPFS()
+            const metadataUrl = file2.ipfs()
+            //interact with smart contract
+            const contract = new web3.eth.Contract(contractABI, contractAddress)
+            const response = await contract.methods
+                .mint(metadataUrl)
+                .send({from: user.get("ethAddress")})
+            const tokenId = response.events.Transfer.returnValues.tokenId
+
+            console.log('TokenId: ', tokenId)
+            console.log('Contract Address',contractAddress)
+
+            alert(`NFT Minted. Contract address = ${contractAddress} and Token ID = ${tokenId}`)
+
+        } catch (err) {
+            console.error(err)
+            alert('Error Miniting NFT')
+        }
     }
   return (
     <div className="flex w-screen h-screen items-center justify-center">
@@ -38,7 +77,6 @@ function Dashboard() {
             <div className='mt-3'>
                <input type="file" className='border-[1px] p-2 text-lg border-black w-full' 
                placeholder="Image/file" 
-               value={file}
                onChange={e => setfile(e.target.files[0])}
                />
             </div> 
